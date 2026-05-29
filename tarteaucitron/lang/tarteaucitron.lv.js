667745e2d16faacbecd81d578b6566336202c0c7
alphaleadership <alphaleadershipyt@protonmail.com>
2026-05-29 09:18:57 +0000

    chore: update build

---
+++ tarteaucitron/lang/tarteaucitron.lv.js
@@ -0,0 +1,95 @@
+/*global tarteaucitron */
+/* min ready */
+tarteaucitron.lang = {
+    "middleBarHead": "☝️ 🍪",
+    "adblock": "Labdien! Izvēlieties, kurus trešo pušu pakalpojumus vēlaties atļaut šajā vietnē.",
+    "adblock_call": "Lai uzsāktu iestatījumu pielāgošanu lūdzam izslēgt reklāmu bloķētāju.",
+    "reload": "Atjaunot lapu",
+
+    "alertBigScroll": "Turpinot ritināt,",
+    "alertBigClick": "Turpinot vietnes izmantošanu,",
+    "alertBig": "Jūs piekrītat trešo pušu pakalpojumiem, kas var izmantot sīkdatnes",
+
+    "alertBigPrivacy": "Šī vietne izmanto sīkdatnes un ļauj jums izvēlēties, kuras no tām pieņemt",
+    "alertSmall": "Pakalpojumu iestatījumi",
+    "personalize": "Pielāgot",
+    "acceptAll": "Pieņemt visu",
+    "close": "Aizvērt",
+    "closeBanner": "Aizvert sīkdatņu joslu",
+
+    "privacyUrl": "Privātuma politika",
+
+    "all": "Iestatījumi visiem pakalpojumiem",
+
+    "info": "Jūsu privātuma aizsardzība",
+    "disclaimer": "Atļaujot šos trešo pušu pakalpojumus, jūs piekrītat sīkdatņu ievietošanai un nolasīšanai, kā arī izsekošanas tehnoloģiju izmantošanai, kas nepieciešamas to pienācīgai darbībai.",
+    "allow": "Atļaut",
+    "deny": "Aizliegt",
+    "noCookie": "Šis pakalpojums neievieto sīkdatnes.",
+    "useCookie": "Šis pakalpojums var ievietot",
+    "useCookieCurrent": "Šis pakalpojums ir ievietojis",
+    "useNoCookie": "Šis pakalpojums nav ievietojis nevienu sīkdatni.",
+    "more": "Uzzināt vairāk",
+    "source": "Skatīt oficiālo vietni",
+    "credit": "Sīkdatņu administrēšana ar tarteaucitron.js",
+    "noServices": "Šī vietne neizmanto nevienu sīkdatni, kurai būtu nepieciešama jūsu piekrišana.",
+
+    "toggleInfoBox": "Rādīt/paslēpt informāciju par sīkdatņu uzglabāšanu",
+    "title": "Sīkdatņu iestatījumu panelis",
+    "cookieDetail": "Sīkdatņu detalizēts apraksts:",
+    "ourSite": "mūsu tīmekļa vietnē",
+    "modalWindow": "(modālais logs)",
+    "newWindow": "(jauna cilne)",
+    "allowAll": "Pieņemt visas",
+    "denyAll": "Noraidīt visas",
+
+    "icon": "Sīkdatnes",
+
+    "fallback": "ir atspējots.",
+    "allowed": "atļauts",
+    "disallowed": "aizliegts",
+
+    "ads": {
+        "title": "Reklāmas pakalpojumu sniedzēji",
+        "details": "Reklāmas tīkli ļauj gūt ieņēmumus, komercializējot vietnes reklāmas laukumus"
+    },
+    "analytic": {
+        "title": "Interneta auditorijas mērījumi",
+        "details": "Apmeklējuma statistikas pakalpojumi ļauj iegūt vietnes apmeklējuma datus, kas palīdz uzlabot tās darbību."
+    },
+    "social": {
+        "title": "Sociālie tīkli",
+        "details": "Sociālie tīkli uzlabo vietnes lietošanas ērtumu un palīdz tās popularizēšanā, izmantojot kopīgošanu."
+    },
+    "video": {
+        "title": "Video",
+        "details": "Video koplietošanas pakalpojumi bagātina vietni ar multivides saturu un palielina tās redzamību."
+    },
+    "comment": {
+        "title": "Komentāri",
+        "details": "Komentāru pārvaldības rīki atvieglo komentāru iesniegšanu un palīdz cīnīties pret surogātpastu."
+    },
+    "support": {
+        "title": "Atbalsts",
+        "details": "Atbalsta rīki ļauj sazināties ar vietnes satura veidotāju komandu un palīdzēt tās uzlabošanā."
+    },
+    "api": {
+        "title": "APIs",
+        "details": "API ļauj ielādēt skriptus: ģeolokāciju, meklētājprogrammas, tulkojumus u.c."
+    },
+    "other": {
+        "title": "Citi",
+        "details": "Pakalpojumi, kas paredzēti tīmekļa satura attēlošanai."
+    },
+
+    "google": {
+        "title": "Īpaša piekrišana Google pakalpojumiem",
+        "details": "Google var izmantot jūsu datus apmeklējuma statistikai, reklāmas efektivitātes novērtēšanai vai personalizētas reklāmas izvēlei."
+    },
+
+    "mandatoryTitle": "Obligātās sīkdatnes",
+    "mandatoryText": "Šī vietne izmanto sīkdatnes, kas ir nepieciešamas tās pienācīgai darbībai. Tās nevar tikt atspējotas.",
+
+    "save": "Saglabāt",
+    "ourpartners": "Mūsu partneri"
+};
